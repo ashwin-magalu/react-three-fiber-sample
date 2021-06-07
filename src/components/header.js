220906b8fa3775572f9cbdbf31a1158bc59f4f80
ashwin-ma <ashwin.magalu@gmail.com>
2021-06-07 13:01:16 +0530

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo">
+        <span>REACT THREE FIBER</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
